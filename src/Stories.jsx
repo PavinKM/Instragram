2eef5989a93a2184ed1db8f69ba5e3253fba3446
PavinKM <pavinyagava@gmail.com>
2025-05-31 20:32:50 +0530

    post page

---
+++ src/Stories.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+function Stories() {
+  return (
+    <div className='story'>Stories</div>
+  )
+}
+
+export default Stories
+
---
+++ src/Suggestions.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+function Suggestions() {
+  return (
+    <div>Suggestions</div>
+  )
+}
+
+export default Suggestions
