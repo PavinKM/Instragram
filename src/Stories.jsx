17b5d64163df3fbcb45d1dc31841624196b4d900
PavinKM <pavinyagava@gmail.com>
2025-06-02 22:15:37 +0530

    story view only

--- src/Stories.jsx
+++ src/Stories.jsx
@@ -1,9 +1,43 @@
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Stories() {
+
+  const [stories, setStories] = useState([])
+
+  useEffect(() => {
+          async function fetchData() {
+            try {
+                const profileRes = await fetch("http://localhost:4000/story");
+                const profileData = await profileRes.json();
+                setStories(profileData);
+              }
+  
+            catch (err) {
+                console.log(err);
+              }
+          }
+          fetchData();
+        }, []);
+
   return (
-    <div className='story'>Stories</div>
+    <div className='story d-flex'>
+      {
+        stories.length > 0 ? 
+        (
+          stories.map((story) => (
+            <div className='story_container' key={story.id}>
+                <div className="gradient-border">
+                  <img src={story.image} alt='dp' className='story-dp rounded-circle'/>
+                </div>
+                <p className="text-truncate" style={{width:"50px"}}>{story.username}</p>
+            </div>
+          ))
+        )
+        :
+        (<p>Loading...</p>)
+      }
+    </div>
   )
 }
 
